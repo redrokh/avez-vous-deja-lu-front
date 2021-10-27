9e734c7ae2fb8ac923447db5c3ff1b2f7058b8e2
redrokh <murday.alexandre@gmail.com>
2021-10-27 15:02:28 +0200

    LatestCarousel component tested inside App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,30 @@
+// Import from libraries
+import { Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+
 // Import components
 import Header from '../Header';
 import Footer from '../Footer';
+import LatestCarousel from '../LatestCarousel';
 
 // Import styles
 import './styles.scss';
 
+// Import data
+import latests from '../../utils/latests';
+
 // == Composant
-const App = () => (
+const App = () => {
+  const [latestIndex, setLatestIndex] = useState(0);
+  const [bestIndex, setBestIndex] = useState(0);
+  return (
   <div className="app">
     <Header />
-    <div>Main</div>
+    <LatestCarousel title="Nos dernières anecdotes" anecdotes={latests} currentIndex={latestIndex} modifyIndex={setLatestIndex} />
     <Footer />
   </div>
-);
+  );
+};
 
 // == Export
 export default App;
