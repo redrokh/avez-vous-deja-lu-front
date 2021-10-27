d15d303e3debe37e23f59543325aa210d74375a0
redrokh <murday.alexandre@gmail.com>
2021-10-27 15:01:05 +0200

    HomeMessage component draft

---
+++ src/components/HomeMessage/index.js
@@ -0,0 +1,11 @@
+const HomeMessage = () => (
+  <section>
+    <h1>Avez-vous déjà lu..?</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet vel lectus eget pulvinar. Vivamus id est est. Nulla facilisi. Nam ultrices dui quis felis interdum, ut aliquet mi malesuada. Aenean rutrum leo sapien, sed ornare eros dignissim et. Vivamus congue leo id tempus blandit. Proin porta turpis id lacus condimentum malesuada. Morbi nec magna ultrices, efficitur tellus eget, dapibus risus. Nullam lobortis, eros a dignissim laoreet, felis ligula pretium nibh, eu porta neque risus nec felis. Nam rutrum orci sed est sollicitudin, eget porta sem feugiat. Integer sed dictum purus. Pellentesque a mauris eleifend, hendrerit urna in, scelerisque felis. Donec suscipit diam id tincidunt suscipit. Duis dictum porttitor massa lacinia maximus.
+    </p>
+  </section>
+);
+
+export default HomeMessage;
