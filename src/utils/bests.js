9e9ac51517cc0890d75dc0ee06e5204133f75fb5
redrokh <murday.alexandre@gmail.com>
2021-10-27 14:59:01 +0200

    data required to test LatestCarousel and BestCarousel components

---
+++ src/utils/bests.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    id: 1,
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing.',
+    writer: {
+      id: 1,
+      pseudo: 'John',
+    },
+    createdAt: '27/10/2021',
+    description: 'Suspendisse tincidunt, ipsum ac convallis pellentesque, lacus sapien molestie libero...',
+    categories: [
+      {
+        id: 1,
+        name: 'Nulla',
+        color: 'lightred',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Proin consequat viverra metus in suscipit.',
+    writer: {
+      id: 2,
+      pseudo: 'Bob',
+    },
+    createdAt: '26/10/2021',
+    description: 'Pellentesque mi enim, dapibus et vestibulum et, convallis ac lorem. Quisque in dignissim eros. Proin a aliquet orci...',
+    categories: [
+      {
+        id: 2,
+        name: 'Mauris',
+        color: 'lightblue',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Pellentesque mi enim, dapibus et vestibulum et, convallis ac lorem.',
+    writer: {
+      id: 2,
+      pseudo: 'Bob',
+    },
+    createdAt: '26/10/2021',
+    description: 'Aliquam dapibus sem at erat mattis dapibus. Sed ac dui mi. Proin convallis, mauris...',
+    categories: [
+      {
+        id: 1,
+        name: 'Nulla',
+        color: 'lightred',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Mauris tempus porta diam vel blandit. Nulla malesuada nibh sit amet enim rutrum congue.',
+    writer: {
+      id: 2,
+      pseudo: 'Bob',
+    },
+    createdAt: '26/10/2021',
+    description: 'Phasellus velit quam, venenatis eget eros sit amet, feugiat maximus ex...',
+    categories: [
+      {
+        id: 2,
+        name: 'Mauris',
+        color: 'lightblue',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Curabitur laoreet magna non quam pellentesque aliquet.',
+    writer: {
+      id: 2,
+      pseudo: 'Bob',
+    },
+    createdAt: '26/10/2021',
+    description: 'Donec elementum turpis id diam bibendum, non aliquam eros semper. Maecenas sed ultrices risus...',
+    categories: [
+      {
+        id: 2,
+        name: 'Mauris',
+        color: 'lightblue',
+      },
+      {
+        id: 1,
+        name: 'Nulla',
+        color: 'lightred',
+      },
+    ],
+  },
+];
