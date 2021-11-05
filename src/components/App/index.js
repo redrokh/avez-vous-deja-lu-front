435acaa22466abe29369b354d9e8c3ae3ffbe06e
Lucas-Camus <lucas.camus36@gmail.com>
2021-11-05 14:47:30 +0100

    Component Contact : Form added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -14,6 +14,7 @@ import LatestCarousel from '../../containers/LatestCarousel';
 import Categories from '../Categories';
 import Anecdotes from '../../containers/Anecdotes';
 import Error from '../Error';
+import Contact from '../Contact';
 
 import categoriesData from '../../utils/categories';
 
@@ -60,6 +61,9 @@ const App = () => (
             title="Nos meilleures anecdotes"
             context="/nos-meilleures-anecdotes"
           />
+        </Route>
+        <Route path="/contact" exact>
+          <Contact />
         </Route>
         <Route>
           <Error />
